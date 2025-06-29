e42b45099049a8c00167d42c0d18d3f16455b125
aminulislamrahat <aminulislamrahat005@gmail.com>
2025-06-29 10:47:54 +0600

    resume added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -145,11 +145,15 @@ export const HeroSection = () => {
           delay: 0.7,
           duration: 0.5
         }}>
-          <motion.a href={`${import.meta.env.BASE_URL}Resume_of_Md_Aminul_Islam_Bhuiyan.pdf`} target="_blank" className="px-6 py-3 bg-blue-600 dark:bg-blue-700 text-white rounded-md hover:bg-blue-700 dark:hover:bg-blue-600 transition-all duration-300 flex items-center gap-2 group" whileHover={{
-            scale: 1.05
-          }} whileTap={{
-            scale: 0.95
-          }}>
+          <motion.a
+            href="https://aminul-islam-rahat-portfolio.surge.sh/Resume_of_Md_Aminul_Islam_Bhuiyan.pdf"
+            // href={`${import.meta.env.BASE_URL}Resume_of_Md_Aminul_Islam_Bhuiyan.pdf`} 
+
+            target="_blank" className="px-6 py-3 bg-blue-600 dark:bg-blue-700 text-white rounded-md hover:bg-blue-700 dark:hover:bg-blue-600 transition-all duration-300 flex items-center gap-2 group" whileHover={{
+              scale: 1.05
+            }} whileTap={{
+              scale: 0.95
+            }}>
             <FileText size={18} className="group-hover:animate-bounce" />
             Download Resume
           </motion.a>
